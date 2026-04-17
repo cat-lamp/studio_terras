17586fc9291d3abb2c7c7837d1fae9620c39479f
Marco Sanchez <marcos>
2026-04-17 14:56:39 -0500

    Fix script path and base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // If using a custom domain, use '/'. 
-  // If using username.github.io/studio-terras/, use '/studio-terras/'
   base: '/', 
 })
